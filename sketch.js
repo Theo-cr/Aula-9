d6de7e39a6eef14517b56b9ad37d2b7750f4eb26
Theo-cr <100632582+Theo-cr@users.noreply.github.com>
2022-02-28 18:43:30 -0300

    V1

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var quadrado;
+
+function setup() {
+  createCanvas(700,600);
+  quadrado=createSprite(350,300,15,15)
+}
+
+function draw() 
+{
+  background("Pink");
+ if (keyDown("up")) {
+ quadrado.y=quadrado.y-2
+ 
+ }
+ if (keyDown("down")) {
+  quadrado.y=quadrado.y+2
+  
+  }
+  if (keyDown("right")) {
+    quadrado.x=quadrado.x-2
+    
+    }
+    if (keyDown("left")) {
+     quadrado.x=quadrado.x+2
+     
+     }
+drawSprites()
+}
+
+
+
+
